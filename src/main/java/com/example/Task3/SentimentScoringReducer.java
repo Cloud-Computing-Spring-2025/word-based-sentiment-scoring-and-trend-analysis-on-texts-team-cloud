40e17e7164cd11b7a00456c18b95d4937da3f6dd
Pavan <pavandantu18@gmail.com>
2025-03-17 02:26:33 -0400

    Task3

---
+++ src/main/java/com/example/Task3/SentimentScoringReducer.java
@@ -0,0 +1,18 @@
+package com.example.Task3;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import java.io.IOException;
+
+public class SentimentScoringReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+         throws IOException, InterruptedException {
+        int cumulativeScore = 0;
+        for (IntWritable val : values) {
+            cumulativeScore += val.get();
+        }
+        context.write(key, new IntWritable(cumulativeScore));
+    }
+}
